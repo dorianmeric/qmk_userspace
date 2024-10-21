e21def00984f469c8121b32aeff5e31d933ed44b
dorianmeric <d.meric@gmail.com>
2024-10-21 21:59:50 +0100

    update (auto)

--- users/dorian-34/layers.h
+++ users/dorian-34/layers.h
@@ -44,20 +44,20 @@ U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),TH1,               TH2,
 KC_HOME,           LCTL(KC_0),        KC_F5,             KC_END,            KC_PGUP,                    KC_WWW_BACK,       KC_WWW_FORWARD,    KC_TAB,            KC_SPC,            KC_BSPC,             \
 KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           KC_PGDN,                    xxxx,              KC_LEFT,           KC_DOWN,           KC_UP,             KC_RGHT,           \
 KC_END,            A(KC_F4),          KC_F2,             KC_LEFT,           KC_RGHT,                    xxxx,              KC_HOME,           KC_PGDN,           KC_PGUP,           KC_END,            \
-U_NP,              U_NP,              xxxx,              xxxx,              xxxx,                       TH3,               KC_ENT,            KC_DEL,            U_NP,              U_NP
+U_NP,              U_NP,              xxxx,              TH1,               TH2,                        TH3,               KC_ENT,            KC_DEL,            U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_MOUSE \
 DM_REC1,           DM_PLY1,           DM_REC2,           DM_PLY2,           DM_RSTP,                    KC_WWW_BACK,       KC_WWW_FORWARD,    xxxx,              xxxx,              JIGGLE,             \
 KC_LGUI,           KC_LALT,           KC_LCTL,           C(KC_S),           LSFT(KC_F9),                KC_BTN3,           KC_MS_L,           KC_MS_D,           KC_MS_U,           KC_MS_R,           \
 xxxx,              RSFT(KC_HOME),     KC_F9,        QK_CAPS_WORD_TOGGLE,    RSFT(KC_END),               KC_BTN2,           KC_WH_L,           KC_WH_D,           KC_WH_U,           KC_WH_R,           \
-U_NP,              U_NP,              xxxx,              xxxx,              TH2,                        KC_BTN1,           TH4,               KC_BTN3,           U_NP,              U_NP
+U_NP,              U_NP,              xxxx,              TH1,               TH2,                         KC_BTN1,           TH4,               KC_BTN3,           U_NP,              U_NP
 
 
 #define MIRYOKU_ALTERNATIVES_MEDIA \
 KC_NUM_LOCK,      E_TREMA,            I_TREMA,           U_TREMA,            xxxx,                      A_GRAVE,           E_AIGU,           E_GRAVE,           E_CIRC,           C_CEDILLE,           \
 KC_INS,           xxxx,               xxxx,              KC_LSFT,           xxxx,                       A_CIRC,     KC_MEDIA_PREV_TRACK,   KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP,   KC_MEDIA_NEXT_TRACK,   \
 KC_CAPS_LOCK,     xxxx,               A_GRAVE,            E_AIGU,      KC_MEDIA_PLAY_PAUSE,             O_CIRC,            I_CIRC,           U_CIRC,           U_GRAVE,          xxxx,              \
-U_NP,               U_NP,              xxxx,              xxxx,              xxxx,                      KC_MUTE,       KC_MEDIA_PLAY_PAUSE, KC_MEDIA_STOP,     U_NP,              U_NP
+U_NP,               U_NP,              xxxx,              TH1,               TH2,                       KC_MUTE,       KC_MEDIA_PLAY_PAUSE, KC_MEDIA_STOP,     U_NP,              U_NP
 
 // Keycodes for manipulating the RGB.
 // RGB_TOG,           RGB_MOD,           RGB_HUI,           RGB_SAI,           RGB_VAI,  
@@ -90,7 +90,7 @@ U_NP,              U_NP,              xxxx,              TH1,               TH2,
 KC_F10,            KC_F7,             KC_F8,             KC_F9,             KC_PSCR,                    KC_PRINT_SCREEN,   KC_F13,            KC_F14,            KC_ALGR,           xxxx,            \
 KC_F11,            KC_F4,             KC_F5,             KC_F6,             KC_SCRL,                    xxxx,              KC_RSFT,           KC_RCTL,           KC_LALT,           KC_RGUI,           \
 KC_F12,            KC_F1,             KC_F2,             KC_F3,             KC_PAUS,                    xxxx,              xxxx,              xxxx,           DF(U_GAME),        DF(U_BASE),              \
-U_NP,              U_NP,              xxxx,              KC_TAB,            KC_SPC,                     xxxx,              xxxx,              xxxx,              U_NP,              U_NP
+U_NP,              U_NP,              xxxx,              KC_TAB,            KC_SPC,                     TH3,               TH4,              xxxx,              U_NP,              U_NP
 
 
 
@@ -116,7 +116,7 @@ U_NP,              U_NP,           xxxx,                KC_KP_0,            KC_K
 S(KC_Q),           S(KC_W),         S(KC_F),            S(KC_P),            S(KC_B),                    S(KC_J),            S(KC_L),            S(KC_U),            S(KC_Y),            S(KC_SLSH),           \
 S(KC_A),           S(KC_R),         S(KC_S),            S(KC_T),            S(KC_G),                    S(KC_M),            S(KC_N),            S(KC_E),            S(KC_I),            S(KC_O),      \
 S(KC_Z),           S(KC_X),         S(KC_C),            S(KC_D),            S(KC_V),                    S(KC_K),            S(KC_H),            KC_COMM,            KC_DOT,             KC_QUOT,   \
-U_NP,              U_NP,            xxxx,               xxxx,               xxxx,                       xxxx,               xxxx,               xxxx,               U_NP,               U_NP
+U_NP,              U_NP,            xxxx,               TH1,                TH2,                        TH3,               TH4,               xxxx,               U_NP,               U_NP
 
 
 
@@ -124,4 +124,4 @@ U_NP,              U_NP,            xxxx,               xxxx,               xxxx
 S_Q,                S_W,            S_F,            S_P,            S_B,                    S_J,            S_L,            S_U,           S_Y,             S_SLSH,           \
 S_A,                S_R,            S_S,            S_T,            S_G,                    S_M,            S_N,            S_E,           S_I,             S_O,      \
 S_Z,                S_X,            S_C,            S_D,            S_V,                    S_K,            S_H,            S_COMM,        S_DOT,           S_QUOT,   \
-U_NP,               U_NP,           xxxx,           xxxx,           xxxx,                  xxxx,            xxxx,           xxxx,           U_NP,           U_NP
+U_NP,               U_NP,           xxxx,           TH1,            TH2,                    TH3,               TH4,           xxxx,           U_NP,           U_NP
